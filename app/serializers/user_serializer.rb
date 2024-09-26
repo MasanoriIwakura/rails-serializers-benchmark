05d8210cc46695a9ce69fce6a44127659953ddac
MasanoriIwakura <m.iwakura@tsukulink.co.jp>
2024-09-26 16:08:50 +0900

    Add schema information to serializers

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  email      :string
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :email
   has_many :profiles, serializer: ProfileSerializer
